b13e49790e121f11b91be78034df876950fce652
Martin Kröning <mkroening@posteo.net>
2021-04-09 16:30:34 +0200

    Revert "Remove stabilized feature"
    This reverts commit 15fda2342c06a4f3859cbf31125325e01d0f402d.

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,7 @@ for all possible configuration options.
 
 #![cfg_attr(not(feature = "builder"), no_std)]
 #![feature(asm)]
+#![feature(unsafe_block_in_unsafe_fn)]
 #![feature(maybe_uninit_extra)]
 #![feature(maybe_uninit_slice)]
 #![deny(unsafe_op_in_unsafe_fn)]
